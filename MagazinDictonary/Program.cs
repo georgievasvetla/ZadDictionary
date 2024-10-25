665b9e805359884d5542932558997a8d47504279
svetla.g <svetlana.georgieva@edu1.pgzaimov.com>
2024-10-25 13:13:20 +0300

    twocommit

--- MagazinDictonary/Program.cs
+++ MagazinDictonary/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            try
+            {
+                var magazin = new Dictionary<string, double>();
+                Console.Write("count=");
+                int count = int.Parse(Console.ReadLine());
+                for (int i = 0; i < count; i++)
+                {
+                    Console.Write("stoka->");
+                    string stoka = Console.ReadLine();
+                    Console.Write("cena->");
+                    double price = double.Parse(Console.ReadLine());
+                    magazin[stoka] = price;
+                }
+                foreach (var etam in magazin)
+                {
+                    Console.WriteLine($"{etam.Key}->{etam.Value}");
+                }
+                using (StreamWriter steam = new StreamWriter("magazin.txt", true))
+                {
+                    foreach (var iteam in magazin)
+                    {
+                        Console.WriteLine($"Stoka:{iteam.Key}->Price:{iteam.Value}");
+                    }
+                }
+            }
+            catch(FileNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(DirectoryNotFoundException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
         }
     }
 }
